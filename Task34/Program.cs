9007188b8a51ea094cb37db79eac92f5321fe4c5
gunderak <sergey.gund@gmail.com>
2023-02-19 22:08:11 +0300

    add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = GetArray(5, 100, 999);
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+int oddNumbers = 0;
+foreach (int el in array)//foreach берёт каждый элемент (el) по заданному условию (el==n или el<>n и т.д.)
+//и выводит результат, который мы хотим в теле цикла if else. Ему не нужно задавать границы, т.к. они у нас уже заданы ранее условиями массива.
+{
+    if (el % 2 == 0)
+    {
+        oddNumbers += 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве = {oddNumbers}.");
